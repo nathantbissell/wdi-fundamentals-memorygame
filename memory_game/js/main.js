1e16c1ac18380adb5540500e6ce0eb1040fa3e5f
nathantbissell <nathantbissell@gmail.com>
2018-10-02 13:20:45 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,22 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-        
+        console.log("You found a match!");
+    }   
+    else {
+    console.log("Sorry, try again.");
     }
-    else {alert("Sorry, try again");}
-}
+    
+};
+
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    
+    checkForMatch();
+    };
+
+flipCard(0);
+flipCard(2);

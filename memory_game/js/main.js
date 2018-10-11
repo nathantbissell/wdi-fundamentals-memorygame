0bf4b6090e9dfe913dc8b9482be2a804216d27f5
nathantbissell <nathantbissell@gmail.com>
2018-10-11 16:20:03 -0400

    added randomized cards and working on reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -62,3 +62,21 @@ var flipCard = function() {
 }
 
 createBoard();
+
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+}
+console.log(shuffle(cards));
